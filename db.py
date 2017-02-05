2e96906d034a9ed3136006f80f22c2de81107f92
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-02-05 15:47:57 +0100

    Make it easier to change the parameters by just passing a dict instead.

--- db.py
+++ db.py
@@ -4,6 +4,10 @@ import config
 def create_connection_string(db_host, db_name, db_user, db_passwd):
     return "host='{}' dbname='{}' user='{}' password='{}'".format(db_host, db_name, db_user, db_passwd)
 
-def connect_to_db(db_host, db_name, db_user, db_passwd):
+def connect_to_db(db_config):
+    db_host = db_config.get("DB_HOST")
+    db_name = db_config.get("DB_NAME")
+    db_user = db_config.get("DB_USER")
+    db_passwd = db_config.get("DB_PASSWD")
     conn = psycopg2.connect(create_connection_string(db_host, db_name, db_user, db_passwd))
     print conn
