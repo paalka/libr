aa816e233df505f3abc7a3333461d14a58a9499c
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-02-05 15:50:25 +0100

    Add a simple helper function which can be used to execute arbitary queries.
    Currently the input parameters are assumed to be sanitized by the caller.

--- db.py
+++ db.py
@@ -11,3 +11,10 @@ def connect_to_db(db_config):
     db_passwd = db_config.get("DB_PASSWD")
     conn = psycopg2.connect(create_connection_string(db_host, db_name, db_user, db_passwd))
     print conn
+
+def execute_query(db_handle, query, parms=None):
+    cursor = db_handle.cursor()
+
+    cursor.execute(query, parms)
+
+    return cursor.fetchall()
