5b137d460b186d38f991920e6c9279a6c0b402b3
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-02-09 01:31:41 +0100

    Use two seperate functions for selecting and inserting rows.

--- db.py
+++ db.py
@@ -11,8 +11,13 @@ def connect_to_db(db_config):
     conn = psycopg2.connect(create_connection_string(db_host, db_name, db_user, db_passwd))
     return conn
 
-def execute_query(db_handle, query, parms=None):
+def execute_select_query(db_handle, query, parms=None):
     cursor = db_handle.cursor()
     cursor.execute(query, parms)
 
     return cursor.fetchall()
+
+def execute_insert_query(db_handle, query, parms=None):
+    cursor = db_handle.cursor()
+    cursor.execute(query, parms)
+    db_handle.commit()
