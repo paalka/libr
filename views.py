4ee7e3cb747ef89978f70aa0fe610d08a5b5c688
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-02-11 14:30:10 +0100

    Allow the user to specify the tags for the uploaded file.

--- views.py
+++ views.py
@@ -25,37 +25,41 @@ def search():
 def find_matching_files(query):
     db_handle = connect_to_db(app.config)
     search_query = """
-                   SELECT file.title, file.filepath
+                   SELECT file.title, file.tags
                    FROM libr.file
-                   WHERE file.title ILIKE %s OR file.filepath ILIKE %s;
+                   WHERE file.title ILIKE %s OR file.tags ILIKE %s;
                    """
     user_query = "%" + query + "%"
     all_files = execute_select_query(db_handle, search_query, (user_query, user_query))
     return all_files
 
-def add_file(filename, filepath):
+def add_file(filename, tags, filepath):
     db_handle = connect_to_db(app.config)
     insert_query = """
-                   INSERT INTO libr.file (title, filepath) VALUES (%s, %s);
+                   INSERT INTO libr.file (title, filepath, tags) VALUES (%s, %s, %s);
                    """
-    execute_insert_query(db_handle, insert_query, (filename, filepath))
+    execute_insert_query(db_handle, insert_query, (filename, filepath, tags))
 
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+
         # check if the post request has the file part
         if 'file' not in request.files:
             return redirect(request.url)
         uploaded_file = request.files['file']
         file_title = request.form.get("title")
+        tags = request.form.get("tags")
+
         # if user does not select file, browser also
         # submit a empty part without filename
         if uploaded_file.filename == '':
             return redirect(request.url)
+
         if uploaded_file and allowed_file(uploaded_file.filename):
             filepath = secure_filename(uploaded_file.filename)
-            add_file(file_title, filepath)
+            add_file(file_title, tags, filepath)
             uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filepath))
             return redirect(url_for('index'))
 
