592b64f75642b2665d402efdae9b7f1f979ceee8
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-02-17 11:10:46 +0100

    Move the config to a object instead of a dict, as this makes it easier to edit and read.

--- libr.py
+++ libr.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from settings import CONFIG_DICT
 from db import init_dbh
 
 app = Flask(__name__)
 import views
-app.config.update(CONFIG_DICT)
+app.config.from_object("libr.settings.LocalConfig")
+
 init_dbh(app)
--- settings/base.py
+++ settings/base.py
@@ -1,8 +1,7 @@
-CONFIG_DICT = {
-    "DB_PASSWD" : "123abc",
-    "DB_USER" : "libr",
-    "DB_HOST" : "localhost",
-    "DB_NAME" : "libr",
-    "UPLOAD_FOLDER": "/home/paal/dev/python/libr/uploads",
-    "ALLOWED_EXTENSIONS": set(["pdf"])
-}
+class BaseConfig(object):
+    DB_PASSWD = "123abc"
+    DB_USER = "libr"
+    DB_HOST = "localhost"
+    DB_NAME = "libr"
+    UPLOAD_FOLDER = "/home/paal/dev/python/libr/uploads"
+    ALLOWED_EXTENSIONS = set(["pdf"])
