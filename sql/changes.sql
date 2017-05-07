a9b03c27b2a602fbd062d83fbedc5d604ccf656c
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-05-07 12:29:32 +0200

    Ensure that the correct column name is used.

--- sql/changes.sql
+++ sql/changes.sql
@@ -1,4 +1,4 @@
 SET SEARCH_PATH TO libr;
 BEGIN;
-  ALTER TABLE libr.category ADD CONSTRAINT category_must_be_unique UNIQUE(libr.category);
+  ALTER TABLE libr.category ADD CONSTRAINT category_must_be_unique UNIQUE(title);
 COMMIT;
