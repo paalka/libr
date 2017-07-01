255284145e3ca349da3f7c2530dab820b1da18a0
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-07-01 20:08:38 +0200

    Ensure that the code works with python3.

--- forms.py
+++ forms.py
@@ -1,13 +1,13 @@
 from wtforms import StringField, FileField, TextAreaField, SelectField, validators
-from flask_wtf import FlaskForm
+from flask_wtf import Form
 from flask_wtf.file import FileRequired
 from models.file import allowed_file
 
-class FileForm(FlaskForm):
+class FileForm(Form):
     file_title = StringField('Title', [validators.Length(min=4), validators.required()])
     categories = SelectField('Category', [validators.required()], coerce=int)
     uploaded_file = FileField("PDF file", [FileRequired()])
     tags = TextAreaField('Tags')
 
-class CategoryForm(FlaskForm):
+class CategoryForm(Form):
     category_title = StringField('Category', [validators.Length(min=2, max=70), validators.required()])
--- settings/__init__.py
+++ settings/__init__.py
@@ -1,6 +1,6 @@
-from base import *
+from .base import *
 
 try:
-        from local import *
-except ImportError, e:
+        from .local import *
+except ImportError:
         raise ImportError("Couldn't load local settings")
