4841c4b1229729a7edfdf54aa8e03a8bf5b24149
Pål Karlsrud <paulkarlsrud@gmail.com>
2018-01-27 21:59:57 +0100

    Only show the edit button when hovering over a file.

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,37 @@
+function bindEditHandlers() {
+    $(".file-edit-wrapper").hide();
+
+    $(".search-result-item").mouseover(function() {
+	var fileId = $(this).attr("data-file-id");
+	$("#file-edit-" + fileId).show();
+    });
+
+    $(".search-result-item").mouseout(function() {
+	var fileId = $(this).attr("data-file-id");
+	$("#file-edit-" + fileId).hide();
+    });
+}
+function updateSearchResults(results) {
+    $("#search-result-container").empty();
+    var results_html = results["html"];
+    if (results_html.length < 10) {
+	var no_res = '' +
+	    '<div class="row search-result-item">' +
+	    '<div class="ten columns">' +
+	    '<h4 class="file-title">No matching files found!</h4></a>' +
+	    '</div>' +
+	    '</div>'
+	results_html = no_res;
+    }
+
+    $("#search-result-container").append(results_html);
+    $(".file-edit-wrapper").hide();
+    bindEditHandlers();
+}
+
 $(function() {
+    bindEditHandlers();
+
     $("#search-field").keyup(function(event) {
 	event.preventDefault();
 	var query = $("#search-field").val();
@@ -14,20 +47,5 @@ $(function() {
 	    }
 	});
     });
-    function updateSearchResults(results) {
-	$("#search-result-container").empty();
-	var results_html = results["html"];
-	if (results_html.length < 10) {
-	    var no_res = '' +
-		'<div class="row search-result-item">' +
-		'<div class="ten columns">' +
-		'<h4 class="file-title">No matching files found!</h4></a>' +
-		'</div>' +
-		'</div>'
-	    results_html = no_res;
-	}
-
-	$("#search-result-container").append(results_html);
-    }
 });
 
