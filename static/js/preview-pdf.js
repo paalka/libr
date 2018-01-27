7319ce16ad2c72a7009b09c3127b9002eec17551
Pål Karlsrud <paulkarlsrud@gmail.com>
2018-01-27 20:12:38 +0100

    Simplify the code used to preview PDFs.

--- static/js/preview-pdf.js
+++ static/js/preview-pdf.js
@@ -1,67 +1,46 @@
 $(function() {
-    var __PDF_DOC,
-	__PAGE_RENDERING_IN_PROGRESS = 0,
-	__CANVAS = $('#pdf-canvas').get(0),
-	__CANVAS_CTX = __CANVAS.getContext('2d');
+    var preview_canvas = $('#pdf-canvas').get(0),
+	preview_canvas_ctx = preview_canvas.getContext('2d');
 
     function showPDF(pdf_url) {
-	$("#pdf-loader").show();
-
 	PDFJS.getDocument({ url: pdf_url }).then(function(pdf_doc) {
-	    __PDF_DOC = pdf_doc;
-	    
-	    // Hide the pdf loader and show pdf container in HTML
-	    $("#pdf-loader").hide();
-	    $("#pdf-contents").show();
+	    var maybeMetadata = pdf_doc.getMetadata().then(function(res) { return res["metadata"] });
 
 	    // Show the first page
-	    showPage(1);
+	    showPage(pdf_doc, 1);
 	}).catch(function(error) {
-	    // If error re-show the upload button
-	    $("#pdf-loader").hide();
-	    $("#upload-button").show();
-	    
 	    alert(error.message);
 	});;
     }
 
-    function showPage(page_no) {
-	__PAGE_RENDERING_IN_PROGRESS = 1;
-
-	// While page is being rendered hide the canvas and show a loading message
+    function showPage(pdf, page_no) {
+	// While page is being rendered hide the canvas so that the user knows
+	// that something is happening.
 	$("#pdf-canvas").hide();
-	$("#page-loader").show();
 
 	// Fetch the page
-	__PDF_DOC.getPage(page_no).then(function(page) {
-	    // As the canvas is of a fixed width we need to set the scale of the viewport accordingly
-	    var scale_required = __CANVAS.width / page.getViewport(1).width;
+	pdf.getPage(page_no).then(function(page) {
+	    // As the canvas is of a fixed width we need
+	    // to set the scale of the viewport accordingly
+	    var required_scale = preview_canvas.width / page.getViewport(1).width;
 
 	    // Get viewport of the page at required scale
-	    var viewport = page.getViewport(scale_required);
+	    var viewport = page.getViewport(required_scale);
 
-	    // Set canvas height
-	    __CANVAS.height = viewport.height;
+	    // Ensure that the canvas is of the same size as the output PDF
+	    preview_canvas.height = viewport.height;
 
 	    var renderContext = {
-		canvasContext: __CANVAS_CTX,
+		canvasContext: preview_canvas_ctx,
 		viewport: viewport
 	    };
-	    
-	    // Render the page contents in the canvas
-	    page.render(renderContext).then(function() {
-		__PAGE_RENDERING_IN_PROGRESS = 0;
 
-		// Show the canvas and hide the page loader
-		$("#pdf-canvas").show();
-		$("#page-loader").hide();
-	    });
+	    page.render(renderContext).then($("#pdf-canvas").show());
 	});
     }
 
-    // When user chooses a PDF file
     $("#uploaded_file").on('change', function() {
-	// Validate whether PDF
+	// Naively attempt to determine whether this is a PDF
 	if(['application/pdf'].indexOf($("#uploaded_file").get(0).files[0].type) == -1) {
 	    alert('Error: Not a PDF');
 	    return;
